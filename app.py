a56b67f36e029bb90cd9b9ac3f6abea129e903da
HKIT <cherry102301@gmail.com>
2023-11-10 12:28:51 +0900

    json route 제작

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 #  "만나서": 2,
 #  "정말": 3,
 #  "반갑습니다!":1}
-from flask import Flask
+from flask import Flask, render_template, request, Response
 from collections import Counter
 import json
 
@@ -15,4 +15,17 @@ def hello_world():
     user_string = '안녕 안녕 반가워 반가워 반가워 반가워 고양이 고양이 고양이 냐옹 나옹 '
     counter = dict(Counter(user_string))
     result = json.dumps(counter)
-    return result
+    return
+
+@app.get("/count/")
+def count():
+    return render_template('count.html')
+
+@app.post("/result/")
+def result():
+    user_input = request.form['userinput']
+    word_dict = dict(Counter(user_input.split()))
+    result = json.dumps(word_dict)
+    return Response(result,
+                    mimetype='application/json',
+                    headers={'Content-Disposition': 'attachment; filename=count.json'})
